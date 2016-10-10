df80467720ddb0423f72fbf448072b2582745b75
eju-jinyongri <jinyongri@ehousechina.com>
2016-10-10 15:12:26 +0800

    补充了默认时区

--- examples/publish.php
+++ examples/publish.php
@@ -3,10 +3,11 @@
 require("../phpMQTT.php");
 
 	
-$mqtt = new phpMQTT("example.com", 1883, "phpMQTT Pub Example"); //第三个参数是client name 需要具备唯一性的字符串
+$mqtt = new phpMQTT("10.0.60.162", 1883, "1phpMQTT Pub Example"); //第三个参数是client name 需要具备唯一性的字符串
 
 if ($mqtt->connect()) {
-	$mqtt->publish("topicName","Hello World! at ".date("r"),0); //在这设置topicName
+	date_default_timezone_set('PRC');
+	$mqtt->publish("topicTest","Hello World! at ".date("r"),0); //在这设置topicName  例:topicTest
 	$mqtt->close();
 }
 

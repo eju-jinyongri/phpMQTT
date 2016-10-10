49a58256992f70ade005919e8e6377de0632381c
eju-jinyongri <jinyongri@ehousechina.com>
2016-10-10 15:08:25 +0800

    做一些中文注释

--- examples/publish.php
+++ examples/publish.php
@@ -3,10 +3,10 @@
 require("../phpMQTT.php");
 
 	
-$mqtt = new phpMQTT("example.com", 1883, "phpMQTT Pub Example"); //Change client name to something unique
+$mqtt = new phpMQTT("example.com", 1883, "phpMQTT Pub Example"); //第三个参数是client name 需要具备唯一性的字符串
 
 if ($mqtt->connect()) {
-	$mqtt->publish("bluerhinos/phpMQTT/examples/publishtest","Hello World! at ".date("r"),0);
+	$mqtt->publish("topicName","Hello World! at ".date("r"),0); //在这设置topicName
 	$mqtt->close();
 }
 

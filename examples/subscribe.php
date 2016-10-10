a156e72e1c6ac76ccfce577169d9f8db6a6632d9
eju-jinyongri <jinyongri@ehousechina.com>
2016-10-10 15:11:10 +0800

    做了一些中文注释

--- examples/subscribe.php
+++ examples/subscribe.php
@@ -1,15 +1,15 @@
 <?php
-
+//消费者例子
 require("../phpMQTT.php");
 
 	
-$mqtt = new phpMQTT("example.com", 1883, "phpMQTT Sub Example"); //Change client name to something unique
+$mqtt = new phpMQTT("example.com", 1883, "phpMQTT Sub Example"); //第三个参数是client name 不能和其他client重复
 
 if(!$mqtt->connect()){
 	exit(1);
 }
 
-$topics['ferries/IOW/#'] = array("qos"=>0, "function"=>"procmsg");
+$topics['topicName'] = array("qos"=>0, "function"=>"procmsg"); //$topics的键是 topic name 生产者和消费者要一一对应
 $mqtt->subscribe($topics,0);
 
 while($mqtt->proc()){
